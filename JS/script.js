a5f2b85c5aef9a0e4e747d70e8e5f0ab253d6754
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-24 22:25:52 +0200

    added rotating background to the pokemon image and made it look more like a pokedex more styling tomorrow

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 
+
 document.getElementById('run').addEventListener('click', (e) => {
     e.preventDefault();
     getPokemon();
@@ -13,10 +14,8 @@ async function getPokemon() {
     let type = document.getElementById('type');
     let evolutionTarget = document.getElementById('evolution');
     let previousEvoImg = document.getElementById('prevEvoImg');
+    let abilityInfoTarget = document.getElementById('Pokemon-info');
 
-
-
-    
 
     let Api = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
     let Pokemon = await Api.json();
@@ -26,24 +25,31 @@ async function getPokemon() {
     movesList.innerHTML = '';
     if (Pokemon.moves.length > 4) {
             for (let i = 0; i < 4; i++) {
-                movesList.innerHTML +=`<li> ${Pokemon.moves[i].move.name} </li>`;
+                movesList.innerHTML +=`<li class="moveLi"> ${Pokemon.moves[i].move.name} </li>`;
             }
         }
     else {
         for (let i = 0; i < Pokemon.moves.length; i++) {
-            movesList.innerHTML += `<li>${Pokemon.moves[i].move.name}</li>`;
+            movesList.innerHTML += `<li class="moveLi">${Pokemon.moves[i].move.name}</li>`;
     }
+    }
+    abilityInfoTarget.innerHTML = ''
+    for (let i = 0; i < Pokemon.abilities.length; i++) {
+        abilityInfoTarget.innerHTML += `<li class="abilityLi">${Pokemon.abilities[i].ability.name}</li>`;
+        console.log(Pokemon.abilities[i].ability.name);
     }
 
-    type.innerHTML = Pokemon.types[0].type.name;
-    
+    type.innerHTML = ''
+    for ( let i = 0; i < Pokemon.types.length; i++) {
+        type.innerHTML += `<li>${Pokemon.types[i].type.name}</li>`;
+    }    
     let EvoApi = await fetch(Pokemon.species.url);
     let evolution = await EvoApi.json();
     try {
         let prevEvoApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${evolution.evolves_from_species.name}`)
         let prevEvolution = await prevEvoApi.json()
         evolutionTarget.innerHTML = '';
-        evolutionTarget.innerHTML += `<h3> Evolves from </h3> ${evolution.evolves_from_species.name}`;
+        evolutionTarget.innerHTML += `<h3> Evolves from <span id='preFormPoke'>${evolution.evolves_from_species.name}</span></h3>`;
         previousEvoImg.setAttribute('src', prevEvolution.sprites.other.home.front_default);
     }
     catch(error) {
@@ -56,6 +62,5 @@ async function getPokemon() {
 }
 
 });
-
 
 
